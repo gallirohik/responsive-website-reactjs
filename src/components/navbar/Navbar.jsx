78963b18755ba6680ba9beffc5246ae777764bf4
Rohik Galli <rohikgalli@Rohiks-MacBook-Pro.local>
2022-01-21 02:25:53 +0530

    responsive navbar design completed.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -35,32 +35,37 @@ const Navbar = () => {
         <div className="gpt3__navbar-links_container">
           <Menu />
         </div>
-        <div className="gpt3__navbar-sign">
-          <p>Sign in</p>
-          <button type="button">Sign up</button>
-        </div>
-        <div className="gpt3__navbar-menu">
-          {toggleMenu ? (
-            <RiCloseLine
-              color="#fff"
-              size={27}
-              onClick={() => setToggleMenu(false)}
-            />
-          ) : (
-            <RiMenu3Line
-              color="#fff"
-              size={27}
-              onClick={() => setToggleMenu(true)}
-            />
-          )}
-          {toggleMenu && (
-            <div className="gpt3__navbar-menu_container scale-up-container">
-              <div className="gpt3__navbar-links_container">
-                <Menu />
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-container">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
               </div>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
